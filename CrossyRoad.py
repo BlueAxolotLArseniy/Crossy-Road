7ca67241f57dba8b9b01481088ae945c976c2a0c
BlueAxolotLArseniy <131585010+BlueAxolotLArseniy@users.noreply.github.com>
2024-02-11 16:39:55 +0300

    v0.010
    I don't know what i can say)

--- CrossyRoad.py
+++ CrossyRoad.py
@@ -7,12 +7,15 @@ import sys
 
 #start
 
+objects = 3
+last_key = None
 wait_car = 0
 gen = 15
 camera_speed = 0
 app = Ursina()
 list_of_roads = []
 list_of_cars = []
+list_of_tree = []
 
 playerX = Entity(model='models/player/chicken/chiken.glb', scale=(.2, .2, .2), position=(1.7, -0.5, -8.5), collider='box')
 playerX.rotation_y = 180
@@ -42,7 +45,7 @@ class Car():
 class Tree():
     def __init__(self, z, type, x):
         if type == '2':
-            self.xmodel = Entity(model='models/tree/defolte/tree2/tree2.glb', scale=(.3, .3, .3), position=(x, -0.9, z+1), collider='box')
+            self.xmodel = Entity(model='models/tree/defolte/tree2/tree2.glb', scale=(.2, .2, .2), position=(x-0.4, -0.9, z+0.4), collider='box')
             self.xmodel.rotation_y = 180
 
 #models and others
@@ -53,13 +56,17 @@ Acar.reparentTo(player)
 #funcs
 
 def generation():
-    global gen, camera
+    global gen, camera, objects
     if random.randint(0, 2) != 0:
         Road(gen)
         list_of_roads.append(gen)
+        objects += 1
     else:
         Grass(gen)
-        Tree(gen, '2', random.randint(-3, 6)+.7)
+        Tree(gen, '2', random.randint(-3, 6))
+        Tree(gen, '2', random.randint(-3, 6))
+        Tree(gen, '2', random.randint(-3, 6))
+        objects += 4
     gen += 1
 
 def movecamera():
@@ -76,28 +83,36 @@ for i in range(5, 20):
 for i in range(-4, 15):
     if random.randint(0, 1) == 0:
         Road(i)
+        objects += 1
     else:
         Grass(i)
+        objects += 1
 
 def input(key):
+    global last_key
     if key == 'left mouse down':
+        last_key = 'w'
         generation()
         playerX.z += 1
         playerX.rotation_y = 180
     if key == 's':
+        last_key = 's'
         generation()
         playerX.z -= 1
         playerX.rotation_y = 0
     if key == 'w':
+        last_key = 'w'
         generation()
         playerX.z += 1
         playerX.rotation_y = 180
     if key == 'a':
+        last_key = 'a'
         generation()
         if playerX.x > -3.7:
             playerX.x -= 1
             playerX.rotation_y = 90
     if key == 'd':
+        last_key = 'd'
         generation()
         if playerX.x < 6.7:
             playerX.x += 1
@@ -107,7 +122,7 @@ def input(key):
 
 
 def update():
-    global test_cube, wait_car
+    global test_cube, wait_car, objects
     movecamera()
     camera.z += 0.01
     if playerX.z - camera.z < 10:
@@ -117,6 +132,7 @@ def update():
         for i in list_of_roads:
             if random.randint(0, 9) == 0:
                 q = Car(i, 'red')
+                objects += 1
                 list_of_cars.append(q)
             else:
                 pass
@@ -126,9 +142,16 @@ def update():
     if wait_car % 300:
         for i in list_of_cars:
             i.xmodel.x += 0.1
-    if len(playerX.intersects().entities) > 1 or len(playerX.intersects().hits) > 1:
-        print('interaction')
-    print(distance(playerX, player))
+    if str(playerX.intersects().entities).find("model='models/car/defolte_red/car_red'") != -1:
+        print('пересечение с машиной')
+    if str(playerX.intersects().entities).find("model='models/tree/defolte/tree2/tree2'") != -1:
+        print('пересечение с деревом')
+        if last_key == 'w': playerX.z -= 1
+        if last_key == 's': playerX.z += 1
+        if last_key == 'a': playerX.x += 1
+        if last_key == 'd': playerX.x -= 1
+    # print(distance(playerX, player))
+    print(objects)
 
 
 
